340fd641dfbe16199d1b4cc7c9009da7954c7c33
Shraddha Tripathi <116650223+shrtri@users.noreply.github.com>
2026-02-21 07:35:36 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import Image from "next/Image";
 import styles from "./navbar2.module.css"
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
+      <div className="flex items-center gap=2px">
+         <Image
+           src="logo.png"
+           alt="PathIQ_logo"
+           height={40}
+           width={40}
+           />
+      </div>
       <Link href="/" className={styles.link}>Home | </Link>
       <Link href="/about" className={styles.link}>About</Link>
     </nav>
